e2552267bd1dfc585c16c5e6668cc7c0916960ab
Romte1 <ivan_ni224@hotmail.com>
2024-02-07 13:26:07 -0300

    Optimized the javascript code, cleaned a bunch of lines that weren't really needed, simplified them in just one function that picks all the possible numbers

--- javascript.js
+++ javascript.js
@@ -2,16 +2,7 @@ var operator = '', firstNumber = 0, secondNumber = 0;
 const display = document.querySelector('.display');
 const calcCle = document.querySelector('.calcCle');
 const calcEqu = document.querySelector('.calcEqu');
-const calcNum0 = document.querySelector('.calcNum0');
-const calcNum1 = document.querySelector('.calcNum1');
-const calcNum2 = document.querySelector('.calcNum2');
-const calcNum3 = document.querySelector('.calcNum3');
-const calcNum4 = document.querySelector('.calcNum4');
-const calcNum5 = document.querySelector('.calcNum5');
-const calcNum6 = document.querySelector('.calcNum6');
-const calcNum7 = document.querySelector('.calcNum7');
-const calcNum8 = document.querySelector('.calcNum8');
-const calcNum9 = document.querySelector('.calcNum9');
+const calcNum = document.querySelectorAll('.calcNum');
 const calcDot = document.querySelector('.calcDot');
 const calcSum = document.querySelector('.calcSum');
 const calcSub = document.querySelector('.calcSub');
@@ -22,17 +13,6 @@ calcCle.addEventListener('click', () => {
     display.textContent = '';
     firstNumber = 0;
     secondNumber = 0;
-})
-
-calcNum0.addEventListener('click', () => {
-    if (display.textContent.length > 11){
-        return
-    }
-    if (display.textContent=='+' || display.textContent=='-' || display.textContent=='*' || display.textContent=='/'){
-        operator = display.textContent;
-        display.textContent = '';
-    }
-    display.textContent +='0';
 })
 
 calcDot.addEventListener('click', () => {
@@ -48,103 +28,18 @@ calcDot.addEventListener('click', () => {
     display.textContent +='.' : display.textContent +='';
 })
 
-calcNum1.addEventListener('click', () => {
-    if (display.textContent.length > 11){
-        return
-    }
-    if (display.textContent=='+' || display.textContent=='-' || display.textContent=='*' || display.textContent=='/'){
-        operator = display.textContent;
-        display.textContent = '';
-    }
-    display.textContent +='1';
-})
+calcNum.forEach((num) => {
 
-calcNum2.addEventListener('click', () => {
-    if (display.textContent.length > 11){
-        return
-    }
-    if (display.textContent=='+' || display.textContent=='-' || display.textContent=='*' || display.textContent=='/'){
-        operator = display.textContent;
-        display.textContent = '';
-    }
-    display.textContent +='2';
-})
-
-calcNum3.addEventListener('click', () => {
-    if (display.textContent.length > 11){
-        return
-    }
-    if (display.textContent=='+' || display.textContent=='-' || display.textContent=='*' || display.textContent=='/'){
-        operator = display.textContent;
-        display.textContent = '';
-    }
-    display.textContent +='3';
-})
-
-calcNum4.addEventListener('click', () => {
-    if (display.textContent.length > 11){
-        return
-    }
-    if (display.textContent=='+' || display.textContent=='-' || display.textContent=='*' || display.textContent=='/'){
-        operator = display.textContent;
-        display.textContent = '';
-    }
-    display.textContent +='4';
-})
-
-calcNum5.addEventListener('click', () => {
-    if (display.textContent.length > 11){
-        return
-    }
-    if (display.textContent=='+' || display.textContent=='-' || display.textContent=='*' || display.textContent=='/'){
-        operator = display.textContent;
-        display.textContent = '';
-    }
-    display.textContent +='5';
-})
-
-calcNum6.addEventListener('click', () => {
-    if (display.textContent.length > 11){
-        return
-    }
-    if (display.textContent=='+' || display.textContent=='-' || display.textContent=='*' || display.textContent=='/'){
-        operator = display.textContent;
-        display.textContent = '';
-    }
-    display.textContent +='6';
-})
-
-calcNum7.addEventListener('click', () => {
-    if (display.textContent.length > 11){
-        return
-    }
-    if (display.textContent=='+' || display.textContent=='-' || display.textContent=='*' || display.textContent=='/'){
-        operator = display.textContent;
-        display.textContent = '';
-    }
-    display.textContent +='7';
-})
-
-calcNum8.addEventListener('click', () => {
-    if (display.textContent.length > 11){
-        return
-    }
-    if (display.textContent=='+' || display.textContent=='-' || display.textContent=='*' || display.textContent=='/'){
-        operator = display.textContent;
-        display.textContent = '';
-    }
-    display.textContent +='8';
-})
-
-calcNum9.addEventListener('click', () => {
-    if (display.textContent.length > 11){
-        return
-    }
-    if (display.textContent=='+' || display.textContent=='-' || display.textContent=='*' || display.textContent=='/'){
-        operator = display.textContent;
-        display.textContent = '';
-    }
-    display.textContent +='9';
+    num.addEventListener('click', () => {
+        if (display.textContent.length > 11){
+            return
+        }
+        if (display.textContent=='+' || display.textContent=='-' || display.textContent=='*' || display.textContent=='/'){
+            operator = display.textContent;
+            display.textContent = '';
+        }
+        display.textContent +=num.textContent;
+    })
 })
 
 calcSum.addEventListener('click', () => {
@@ -173,11 +68,9 @@ calcMul.addEventListener('click', () => {
         return
     } else if (operator == '*') {
         firstNumber *= parseFloat(display.textContent);
-        console.log(firstNumber);
         display.textContent ='*';          
     } else {
     firstNumber += parseFloat(display.textContent);
-    console.log(firstNumber);
     display.textContent ='*';
     } 
 })
@@ -187,11 +80,9 @@ calcDiv.addEventListener('click', () => {
         return
     } else if (operator == '/') {
         firstNumber /= parseFloat(display.textContent);
-        console.log(firstNumber);
         display.textContent ='/';          
     } else {
     firstNumber += parseFloat(display.textContent);
-    console.log(firstNumber);
     display.textContent ='/';
     } 
 })
@@ -222,34 +113,13 @@ calcEqu.addEventListener('click', () => {
 
 function operate(operator,a,b){
     if (operator === '+'){
-       return add(a,b);
+       return a+b;
     } else if (operator ==='-'){
-        return subtract(a,b);
+        return a-b;
     } else if (operator ==='*'){
-        return multiply(a,b);
+        return a*b;
     } else if (operator ==='/'){
-        return divide(a,b);
+        return a/b;
     }
 
 }
-
-function add(a,b){
-    return a+b;
-}
-
-function subtract(a,b){
-    return a-b;
-}
-
-function multiply(a,b){
-    return a*b;
-}
-
-function divide(a,b){
-    return a/b;
-}
-// console.log(add(a,b));
-// console.log(subtract(a,b));
-// console.log(multiply(a,b));
-// console.log(divide(a,b));
-
